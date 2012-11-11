4c1e5f9eec9d3c9cae29114b3197154fffaf8b5d
Milan Rukavina <rukavinamilan@gmail.com>
2012-11-11 09:25:14 +0100

    check 80 port

--- js/front.js
+++ js/front.js
@@ -7,7 +7,7 @@
  *
  */
 //UPDATE this var
-var wsURL = 'ws://tetatet-rukavina.dotcloud.com:8080';
+var wsURL = 'ws://tetatet-rukavina.dotcloud.com';
 
 //connect to ws server
 function nodeLogin(userInfo){
